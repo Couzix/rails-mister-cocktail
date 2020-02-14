86235f3265a904e0578a24e6cfdc6331ce4850ba
Paul Couzinet <couzinet.paul@gmail.com>
2020-02-14 12:01:49 +0100

    deploy

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const { environment } = require('@rails/webpacker')
+
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
@@ -9,5 +9,4 @@ environment.plugins.prepend('Provide',
   })
 )
 
-module.exports = environment
 module.exports = environment
